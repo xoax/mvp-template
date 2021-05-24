ae22f8e0346a71d6e915662132e8b1bb9c98da78
GitHub Action <action@github.com>
2021-05-24 01:29:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/xoax/mvptemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xoax/mvptemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xoax.mvptemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xoax.mvptemplate.MyBundle
 
 class MyApplicationService {
 
